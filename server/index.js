017b9b8011943d7c256e4d6a0fe2733cf1f393de
Rahul Kumar Mishra <rahulmishra21460@gmail.com>
2023-10-01 17:03:39 +0530

    server initialized in index.js

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+
+const PORT = 5500;
+
+app.listen(PORT, () => {
+    console.log("Listening on Port " + PORT + "...")
+})
